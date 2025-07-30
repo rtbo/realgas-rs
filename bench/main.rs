d46d5abf0f4fb8b45c056fbf9cda02661fa48091
Rémi Thebault <remi.thebault@gmail.com>
2025-07-30 12:18:11 +0200

    Adding bench for H2 and N2

--- bench/main.rs
+++ bench/main.rs
@@ -1,12 +1,18 @@
 use realgas::compounds;
 
-
 mod zbench;
 
+const EXP_Z_H2_CSV: &str = include_str!("exp/z_h2.csv");
+const EXP_Z_N2_CSV: &str = include_str!("exp/z_n2.csv");
 const EXP_Z_AIR_CSV: &str = include_str!("exp/z_air.csv");
 
 fn main() {
-    let air = compounds::dry_air().into();
+    let h2 = compounds::H2.into();
+    zbench::do_gas(EXP_Z_H2_CSV, "H2", &h2, &[40.0, 300.0, 2000.0]);
 
+    let n2 = compounds::N2.into();
+    zbench::do_gas(EXP_Z_N2_CSV, "N2", &n2, &[80.0, 300.0, 1000.0]);
+
+    let air = compounds::dry_air().into();
     zbench::do_gas(EXP_Z_AIR_CSV, "air", &air, &[100.0, 300.0, 1000.0]);
 }
